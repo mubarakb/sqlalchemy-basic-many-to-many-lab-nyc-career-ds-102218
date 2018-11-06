ef18ef2a47145a59b74bf34f5a7043dd32ce87c5
flatiron school <flatironschool@flatirons-MacBook-Air-5.local>
2018-11-06 16:32:40 -0500

    updated results

--- queries.py
+++ queries.py
@@ -1,13 +1,14 @@
 from models import *
 
 def return_christian_bales_roles(session):
-    pass
+    bale= session.query(Actor).filter_by(name= 'Christian Bale').first()
+    return bale.roles
     # Return a list of Christian Bale role instances
 
 def return_catwoman_actors(session):
-    pass
+    return session.query(Role).filter_by(character= 'Catwoman').first().actors
     #  Return a list of actor instances that have played Catwoman
 
 def return_number_of_batman_actors(session):
-    pass
+    return len(session.query(Role).filter_by(character= 'Batman').first().actors)
     # Return the number of actors that have played Batman
